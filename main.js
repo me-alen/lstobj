44cc0e5082e0ffe5ad1bbf64e8366ddabd190e51
me-alen <alenofficial10@gmail.com>
2019-12-23 16:31:30 +0530

    range on process

---
+++ main.js
@@ -0,0 +1,65 @@
+let elementArray = [];
+function Transfer(leftList, rightList, buttons) {
+  this.leftList = leftList;
+  this.rightList = rightList;
+  this.buttons = buttons;
+  Array.from(leftList.children).forEach(event => {
+    event.addEventListener("click", function() {
+      elementArray.push(event);
+      event.style.backgroundColor = "lightgreen";
+    });
+  });
+  Array.from(rightList.children).forEach(event => {
+    event.addEventListener(function() {
+      elementArray.push(event);
+      event.style.backgroundColor = "lightgreen";
+    });
+  });
+  this.rightButton = buttons.querySelector(".right");
+  this.leftButton = buttons.querySelector(".left");
+  this.rightButton.addEventListener("click", moveRight);
+  this.leftButton.addEventListener("click", moveLeft);
+  function moveRight() {
+    elementArray.forEach(event => {
+      rightList.appendChild(event);
+      event.style.backgroundColor = "";
+      elementArray = [];
+    });
+  }
+  function moveLeft() {
+    elementArray.forEach(event => {
+      leftList.appendChild(event);
+      event.style.backgroundColor = "";
+      elementArray = [];
+    });
+  }
+
+  buttons
+    .querySelector(".rightInputButton")
+    .addEventListener("click", moveIndexToRight);
+  buttons
+    .querySelector(".leftInputButton")
+    .addEventListener("click", moveIndexToLeft);
+
+  function moveIndexToRight() {
+    const fromInput = parseInt(document.querySelector(".fromInput").value);
+    const toInput = parseInt(document.querySelector(".toInput").value);
+    debugger;
+  }
+  function moveIndexToLeft() {
+    const fromInput = parseInt(document.querySelector(".fromInput").value);
+    debugger;
+  }
+}
+
+const firstPairList = new Transfer(
+  document.querySelector(".ullist1"),
+  document.querySelector(".ullist2"),
+  document.querySelector(".buttons12")
+);
+
+const secondPairList = new Transfer(
+  document.querySelector(".ullist2"),
+  document.querySelector(".ullist3"),
+  document.querySelector(".buttons23")
+);
